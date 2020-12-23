9d06f4a9b2941ef5d7bb335778defaaa1fe5e3ff
Toxin-hotel <76273836+Toxin-hotel@users.noreply.github.com>
2020-12-23 10:52:29 +0300

    Create carousel_init.js

---
+++ carousel_init.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.card-slider').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 0,
+        fade: true
+    });
+});
