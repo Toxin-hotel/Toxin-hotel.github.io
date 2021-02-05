8acf211d2fbc86b4d5ba197332e9bab3acf04073
Toxin-hotel <76273836+Toxin-hotel@users.noreply.github.com>
2021-02-05 07:05:09 +0300

    Add files via upload

--- dialog.js
+++ dialog.js
@@ -1,66 +1,49 @@
-const menu_items = document.querySelectorAll('.cm-dropdown-menu__item');
-const enter = document.querySelector('.header__entrance');
+const pins = document.querySelectorAll('button');
+const buttons = Array.from(pins);
 const overlay = document.querySelector('.modal-background');
 
-const dialogOpen = function(event) {
-    console.log(event.target);
-    if (event.target.nodeName == 'BUTTON') {
-        if (event.target.classList.contains('btn--sign-in')) {
-            const current_classes = event.target.className.split(' ');
+const modal_box = document.querySelector('.modal-box');
+/*Open Modal Window*/
+for (const button of buttons) {
+    button.addEventListener('click', (event) => {
+        event.preventDefault;
+        overlay.classList.remove('hidden');
 
-            for (let current of current_classes) {
-                if (current == 'btn--sign-in') {
-                    var class_name = current;
-                }
-            }
+        if (event.target.id == 'short') {
 
-            const dialog = document.querySelector(`#${class_name}`);
-            overlay.classList.remove('hidden');
-            dialog.classList.remove('hidden');
+            const low = modal_box.querySelector('#low');
+            const window_body = low.querySelector('.modal-body');
+            const form = window_body.querySelector('.form');
 
-            const body = dialog.querySelector(`#${class_name} .modal-body`);
-            body.classList.add('rise-modal');
-
-            const form = dialog.querySelector(`#${class_name} .form`);
+            low.classList.remove('hidden');
+            /*the animation*/
+            window_body.classList.add('rise-modal');
             form.classList.add('rise-dialog');
 
-        } else if (event.target.classList.contains('btn--sign-up')) {
-            const current_classes = event.target.className.split(' ');
+        } else if (event.target.id == 'long') {
 
-            for (let current of current_classes) {
-                if (current == 'btn--sign-up') {
-                    var class_name = current;
-                }
-            }
+            const tall = modal_box.querySelector('#tall');
+            const window_body = tall.querySelector('.modal-body');
+            const form = window_body.querySelector('.form');
 
-            const dialog = document.querySelector(`#${class_name}`);
-            overlay.classList.remove('hidden');
-            dialog.classList.remove('hidden');
-
-            const body = document.querySelector(`#${class_name} .modal-body`);
-            body.classList.add('rise-modal');
-
-            const form = dialog.querySelector(`#${class_name} .form`);
+            tall.classList.remove('hidden');
+            /*the animation*/
+            window_body.classList.add('rise-modal');
             form.classList.add('rise-dialog');
+
         }
-    } else return;
+    });
 }
 
-const dialogClose = function() {
-    const dialogs = Array.from(document.querySelectorAll('.modal-window'));
+/*Close Modal Window*/
+overlay.addEventListener('click', () => {
+    const windows = document.querySelectorAll('.modal-window');
+    const modals = Array.from(windows);
 
-    for (let window of dialogs) {
-        if (!window.classList.contains('hidden')) {
-            window.classList.add('hidden');
+    for (const modal of modals) {
+        if (!modal.classList.contains('hidden')) {
+            modal.classList.add('hidden');
             overlay.classList.add('hidden');
         }
     }
-}
-
-enter.addEventListener('click', dialogOpen);
-
-for (const item of menu_items) {
-    item.addEventListener('click', dialogOpen);
-}
-
-overlay.addEventListener('click', dialogClose);
+});
